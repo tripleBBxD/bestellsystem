3c73f93a6390da3fab15bc0879e7c7f8d9a58040
Bjarne Boecker <bjarneboecker@MacBook-Pro-von-Bjarne.local>
2024-05-16 10:45:39 +0200

    set card width and center selected items

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -13,18 +13,12 @@ function Product(
 ) {
 
   const selectProduct = (productName: string) => console.log(`product selected ${productName}`)
-  return (
-
-    <div onClick={() => selectProduct(name)}>
-        <div className="mx-auto px-5 ">
-        <div className="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
-        <img className="w-full rounded-lg ko0ioo9 object-cover object-center" src={productImage} alt="product" />
-        <p className="my-4 pl-4 font-bold text-gray-500">{name}</p>
-        <p className="mb-4 ml-4 text-xl font-semibold text-gray-800">{price}$</p>
-        </div>
-        </div>
+  return (  
+    <div onClick={() => selectProduct(name)} className="w-3/5 cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+      <img className="w-full rounded-lg ko0ioo9 object-cover object-center" src={productImage} alt="product" />
+      <p className="my-4 pl-4 font-bold text-gray-500">{name}</p>
+      <p className="mb-4 ml-4 text-xl font-semibold text-gray-800">{price}$</p>
     </div>
-
   )
 }
 
